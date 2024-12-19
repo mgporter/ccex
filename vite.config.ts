70a22a69d9d67c2796ac264d114659b08d7b3d57
mgporter <26415062+mgporter@users.noreply.github.com>
2024-12-18 22:36:04 -0500

    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ccex/'
+  base: 'https://mgporter.github.io/ccex/'
   // test: {
   //   globals: true,
   //   environment: "jsdom"
